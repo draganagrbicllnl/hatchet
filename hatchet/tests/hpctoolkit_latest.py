b44f832c71b517d98a4583736305e5a114858e00
Dragana Grbic <grbic1@quartz764.llnl.gov>
2024-05-21 15:13:24 -0700

    update: license comments inside test filed

--- hatchet/tests/hpctoolkit_latest.py
+++ hatchet/tests/hpctoolkit_latest.py
@@ -1,3 +1,8 @@
+# Copyright 2017-2023 Lawrence Livermore National Security, LLC and other
+# Hatchet Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
 from hatchet import GraphFrame
 from hatchet.node import Node
 
